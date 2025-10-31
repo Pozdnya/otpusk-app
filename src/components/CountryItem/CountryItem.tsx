617688e1f29fdcc37f588a8704384e81cb267e95
Kyryl Pozdniakov <kyryl.pozdniakov@itsua.eu>
2025-10-31 18:29:09 +0200

    added setting value to input while click to country from dropdown

--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -1,12 +1,21 @@
 import type { FC } from 'react'
 import type { Country } from '../../types'
+import { useAppDispatch } from '../../hooks/redux';
+import { dropdownSlice } from '../../store/reducers/DropdownSlice';
 
 interface Props {
   country: Country
 }
 export const CountryItem: FC<Props> = ({country}) => {
+  const dispath = useAppDispatch();
+  const handleSelect = (event: React.MouseEvent) => {
+    const input = event.target as HTMLInputElement
+
+    dispath(dropdownSlice.actions.setQueryValue(input.innerText));
+  }
+
   return (
-    <div className='country-item'>
+    <div className='country-item' onClick={handleSelect}>
       <img src={country.flag} alt='Flag image' className='country-item__flag'/>
       <p className='country-item__name'>{country.name}</p>
     </div>
